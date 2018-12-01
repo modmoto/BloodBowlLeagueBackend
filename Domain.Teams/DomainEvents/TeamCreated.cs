eed5e44ec10cb04af8a81a99b8dcac777ecdec1f
Simon Heiss <simonheiss87@gmail.com>
2018-12-01 16:10:49 +0100

    fixed newtonsoft bug, with new version, have to reset solution

--- Domain.Teams/DomainEvents/TeamCreated.cs
+++ Domain.Teams/DomainEvents/TeamCreated.cs
@@ -10,9 +10,9 @@ namespace Domain.Teams.DomainEvents
         public string TeamName { get; }
         public string TrainerName { get; }
 
-        public TeamCreated(Guid entityId, Guid raceId, string teamName, string trainerName)
+        public TeamCreated(Guid teamId, Guid raceId, string teamName, string trainerName)
         {
-            EntityId = entityId;
+            EntityId = teamId;
             RaceId = raceId;
             TeamName = teamName;
             TrainerName = trainerName;
