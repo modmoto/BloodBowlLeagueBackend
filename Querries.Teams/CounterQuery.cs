0e6729e71c334653cb2d09e4751f3d67e2f924b1
Simon Heiss <simonheiss87@gmail.com>
2018-12-04 19:47:09 +0100

    comment for windows

--- Querries.Teams/CounterQuery.cs
+++ Querries.Teams/CounterQuery.cs
@@ -11,5 +11,7 @@ namespace Querries.Teams
         {
             CreatedCount = CreatedCount + 1;
         }
+
+        //nuget delete Microwave.Queries 1.0.0.0 -ApiKey e7jrjjfqck2ajelalqvp5sl6 -Source https://ci.appveyor.com/nuget/lauchi-jwih9ne6cq4t/api/v2/package
     }
 }
