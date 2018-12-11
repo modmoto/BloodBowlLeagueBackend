8151419a7abad2cab6e2f20653944a9a593506b3
Simon Heiss <simonheiss87@gmail.com>
2018-12-11 21:30:27 +0100

    updated version, port to netstandard, query model fixed

--- QuerryHost.Teams/Program.cs
+++ QuerryHost.Teams/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microwave.DependencyInjectionExtensions;
+using Microwave.Queries;
 
 namespace QuerryHost.Teams
 {
