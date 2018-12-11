1179e3de9d147f7866c9019e2844fddb15ec7021
Simon Heiss <simonheiss87@gmail.com>
2018-12-11 19:57:19 +0100

    implemented new config, but parallel readmodels not working

---
+++ Querries.Teams/RaceReadModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microwave.Queries;
+using Querries.Teams.DomainEvents;
+
+namespace Querries.Teams
+{
+    public class RaceReadModel : ReadModel, IHandle<RaceCreated>
+    {
+        public Guid RaceId { get; set; }
+        public string RaceDescription { get; set; }
+        public IEnumerable<AllowedPlayer> AllowedPlayers { get; set; }
+
+        public void Handle(RaceCreated domainEvent)
+        {
+            RaceId = domainEvent.EntityId;
+            AllowedPlayers = domainEvent.AllowedPlayers;
+            RaceDescription = domainEvent.RaceDescription;
+        }
+    }
+}
