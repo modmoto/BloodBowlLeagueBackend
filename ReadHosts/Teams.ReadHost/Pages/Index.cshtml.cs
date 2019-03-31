3cfec766c8e265e3c5a580382af8dbc5e4c5e079
Simon Heiss <simonheiss87@gmail.com>
2019-04-01 00:56:57 +0200

    added some bootstrap stuff

--- ReadHosts/Teams.ReadHost/Pages/Index.cshtml.cs
+++ ReadHosts/Teams.ReadHost/Pages/Index.cshtml.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microwave.Application.Discovery;
 
 namespace Teams.ReadHost.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly DiscoveryHandler _discoveryHandler;
+
+        public IEventLocation ConsumingServices { get; set; }
+
+        public IndexModel(DiscoveryHandler discoveryHandler)
+        {
+            _discoveryHandler = discoveryHandler;
+        }
+
         public void OnGet()
         {
+            var consumingServices = _discoveryHandler.GetConsumingServices();
+            ConsumingServices = consumingServices;
+        }
+
+        public async Task DiscoverServices()
+        {
+            await _discoveryHandler.DiscoverConsumingServices();
         }
     }
 }
