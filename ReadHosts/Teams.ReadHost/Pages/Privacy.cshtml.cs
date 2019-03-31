f216a1635a72e50c3c4828fe9e9071516585ef92
Simon Heiss <simonheiss87@gmail.com>
2019-03-31 21:22:33 +0200

    start of ui

---
+++ ReadHosts/Teams.ReadHost/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Teams.ReadHost.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
