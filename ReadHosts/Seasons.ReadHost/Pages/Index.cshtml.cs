772cee96513e7e6c248beb31fccd7791ae3d34a9
Simon Heiss <simonheiss87@gmail.com>
2019-08-08 20:58:55 +0200

    added button and shit

--- ReadHosts/Seasons.ReadHost/Pages/Index.cshtml.cs
+++ ReadHosts/Seasons.ReadHost/Pages/Index.cshtml.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microwave.Domain.Results;
 using Microwave.Queries;
+using Newtonsoft.Json;
 using Seasons.ReadHost.Seasons;
 
 namespace Pages
@@ -21,6 +26,17 @@ namespace Pages
         {
             var loadAll = await _readModelRepository.Load<AllSeasonsOverview>();
             AllSeasons = loadAll.Is<Ok>() ? loadAll.Value : new AllSeasonsOverview();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var seasonName = Request.Form["seasonNameTextInput"];
+            var httpClient = new HttpClient();
+            var teamObject = JsonConvert.SerializeObject(new { seasonName = seasonName.ToString() });
+            var content = new StringContent(teamObject, Encoding.UTF8, "application/json");
+            var requestUri = new Uri("http://localhost:5004/Api/Seasons/create");
+            await httpClient.PostAsync(requestUri, content);
+            return Redirect("http://localhost:5006");
         }
     }
 }
