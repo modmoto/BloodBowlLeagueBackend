ad5c76d55f5f367b0da8a3a20ffbb9a2b416f409
Simon Heiss <simonheiss87@gmail.com>
2019-08-08 22:59:11 +0200

    fn shit

---
+++ ReadHosts/Teams.ReadHost/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microwave.Queries;
+using Newtonsoft.Json;
+using Teams.ReadHost.Races;
+using Teams.ReadHost.Teams;
+
+namespace Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IReadModelRepository _readModelRepository;
+
+        public IEnumerable<TeamOverviewReadModel> AllTeams { get; set; }
+        public IEnumerable<RaceReadModel> AllRaces { get; set; }
+
+        public IndexModel(IReadModelRepository readModelRepository)
+        {
+            _readModelRepository = readModelRepository;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var teams = await _readModelRepository.LoadAll<TeamOverviewReadModel>();
+            var races = await _readModelRepository.LoadAll<RaceReadModel>();
+            AllTeams = teams.Value;
+            AllRaces = races.Value;
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var teamName = Request.Form["teamNameTextInput"];
+            var trainerName = Request.Form["trainerNameTextInput"];
+            var raceId = Request.Form["raceIdDropDownInput"];
+            var httpClient = new HttpClient();
+            var teamObject = JsonConvert.SerializeObject(new { teamName, trainerName, raceId });
+            var content = new StringContent(teamObject, Encoding.UTF8, "application/json");
+            var requestUri = new Uri("http://localhost:5001/Api/Teams/create");
+            await httpClient.PostAsync(requestUri, content);
+            return Redirect("http://localhost:5006");
+        }
+    }
+}
---
+++ ReadHosts/Teams.ReadHost/Pages/Teams.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microwave.Queries;
+using Newtonsoft.Json;
+
+namespace Pages
+{
+    public class Teams : PageModel
+    {
+        private readonly IReadModelRepository _readModelRepository;
+
+        [BindProperty(SupportsGet = true)]
+        public Guid SeasonId { get; set; }
+
+        public Teams(IReadModelRepository readModelRepository)
+        {
+            _readModelRepository = readModelRepository;
+        }
+
+        public async Task OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var teamId = Request.Form["teamId"];
+            var httpClient = new HttpClient();
+            var teamObject = JsonConvert.SerializeObject(new { teamId = teamId.ToString() });
+            var content = new StringContent(teamObject, Encoding.UTF8, "application/json");
+            var requestUri = new Uri($"http://localhost:5004/Api/Seasons/{SeasonId}/add-team");
+            await httpClient.PostAsync(requestUri, content);
+
+            return Redirect(SeasonId.ToString());
+        }
+    }
+}
