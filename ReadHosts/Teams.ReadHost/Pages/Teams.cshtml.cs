430f5255fc336cda6e23e1b656a77d874ea96f3e
Simon Heiss <simonheiss87@gmail.com>
2019-08-11 18:40:21 +0200

    team thingy

--- ReadHosts/Teams.ReadHost/Pages/Teams.cshtml.cs
+++ ReadHosts/Teams.ReadHost/Pages/Teams.cshtml.cs
@@ -1,38 +1,42 @@
 using System;
-using System.Net.Http;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microwave.Domain.Identities;
 using Microwave.Queries;
-using Newtonsoft.Json;
-using ReadHosts.Common;
+using Teams.ReadHost.Players;
+using Teams.ReadHost.Teams;
 
 namespace Teams.ReadHost.Pages
 {
     public class Teams : PageModel
     {
         private readonly IReadModelRepository _readModelRepository;
-        private readonly MessageMitigator _mitigator;
+
+        public TeamReadModel Team { get; set; }
+        public IEnumerable<PlayerReadModel> Players { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public Guid SeasonId { get; set; }
+        public Guid TeamId { get; set; }
+
 
         public Teams(
-            IReadModelRepository readModelRepository,
-            MessageMitigator mitigator)
+            IReadModelRepository readModelRepository)
         {
             _readModelRepository = readModelRepository;
-            _mitigator = mitigator;
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task OnGet()
         {
-            var teamId = Request.Form["teamId"];
-            await _mitigator.PostAsync(
-                new Uri($"http://localhost:5004/Api/Seasons/{SeasonId}/add-team"),
-                new { teamId = teamId.ToString() });
-            return Redirect(SeasonId.ToString());
+            var result = await _readModelRepository.Load<TeamReadModel>(GuidIdentity.Create(TeamId));
+            var players = await _readModelRepository.LoadAll<PlayerReadModel>();
+
+            var team = result.Value;
+            var playerReadModels = players.Value.Where(p => team.PlayerList.Any(t => t.PlayerId == p.PlayerId));
+            Players = playerReadModels;
+            Team = team;
         }
     }
 }
