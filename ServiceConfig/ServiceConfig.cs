83fa164bcebfbd6faa86ca6e999e650575892fae
Simon Heiss <simonheiss87@gmail.com>
2019-08-12 22:06:57 +0200

    added laync

--- ServiceConfig/ServiceConfig.cs
+++ ServiceConfig/ServiceConfig.cs
@@ -10,7 +10,8 @@ namespace ServiceConfig
             new Uri("http://localhost:5001"),
             new Uri("http://localhost:5002"),
             new Uri("http://localhost:5003"),
-            new Uri("http://localhost:5004")
+            new Uri("http://localhost:5004"),
+            new Uri("http://localhost:5007"),
         };
     }
 }
