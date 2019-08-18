439fb38b0c88bd9e983bd18f28c76e2a6e694bb0
Simon Heiss <simonheiss87@gmail.com>
2019-08-19 00:06:39 +0200

    add azure config, see what happens

--- ServiceConfig/ServiceConfig.cs
+++ ServiceConfig/ServiceConfig.cs
@@ -5,13 +5,22 @@ namespace ServiceConfig
 {
     public class ServiceConfiguration
     {
+//        public static ServiceBaseAddressCollection ServiceAdresses => new ServiceBaseAddressCollection
+//        {
+//            new Uri("http://localhost:5001"),
+//            new Uri("http://localhost:5002"),
+//            new Uri("http://localhost:5003"),
+//            new Uri("http://localhost:5004"),
+//            new Uri("http://localhost:5007"),
+//        };
+
         public static ServiceBaseAddressCollection ServiceAdresses => new ServiceBaseAddressCollection
         {
-            new Uri("http://localhost:5001"),
-            new Uri("http://localhost:5002"),
-            new Uri("http://localhost:5003"),
-            new Uri("http://localhost:5004"),
-            new Uri("http://localhost:5007"),
+            new Uri("https://matcheshost.azurewebsites.net"),
+            new Uri("https://teamshost.azurewebsites.net"),
+            new Uri("https://playershost.azurewebsites.net"),
+            new Uri("https://seasonshost.azurewebsites.net"),
+            new Uri("https://raceshost.azurewebsites.net"),
         };
     }
 }
