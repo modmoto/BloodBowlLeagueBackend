55dbdb98db34efb1d0fbab2e3975d86eecac9040
Simon Heiss <simonheiss87@gmail.com>
2019-11-05 06:52:24 +0100

    added more team endpoints

--- ReadHosts/Teams.ReadHost/Teams/TeamQuerryController.cs
+++ ReadHosts/Teams.ReadHost/Teams/TeamQuerryController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microwave.Queries;
+using Teams.ReadHost.Pages;
+using Teams.ReadHost.Players;
 
 namespace Teams.ReadHost.Teams
 {
@@ -20,6 +24,63 @@ namespace Teams.ReadHost.Teams
         {
             var teamQuerry = await _queryRepository.LoadAsync<TeamReadModel>(teamId);
             return Ok(teamQuerry.Value);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetTeamsOverviews()
+        {
+            var teamQuerry = await _queryRepository.LoadAllAsync<TeamReadModel>();
+            var teamOverviewHtos = teamQuerry.Value.Select(t => new TeamOverviewHto(t));
+            return Ok(teamOverviewHtos);
+        }
+
+        [HttpGet("{teamId}/full")]
+        public async Task<ActionResult> GetTeamWithPlayers(Guid teamId)
+        {
+            var result = await _queryRepository.LoadAsync<TeamReadModel>(teamId);
+            var players = await _queryRepository.LoadAllAsync<PlayerReadModel>();
+
+            var team = result.Value;
+            var playersValue = players.Value;
+            var playerReadModels = playersValue.Where(p =>
+                team.PlayerList.Any(a => a.PlayerId == p.PlayerId));
+            return Ok(new TeamWithPlayersHto(team, playerReadModels));
+        }
+    }
+
+    public class TeamWithPlayersHto
+    {
+        public string TeamTeamName { get; }
+        public Guid TeamTeamId { get; }
+        public IEnumerable<PlayerReadModel> PlayerList { get; }
+        public string TeamTrainerName { get; }
+        public string RaceId { get; }
+
+        public TeamWithPlayersHto(TeamReadModel team, IEnumerable<PlayerReadModel> playerReadModels)
+        {
+            TeamTeamName = team.TeamName;
+            TeamTeamId = team.TeamId;
+            TeamTrainerName = team.TrainerName;
+            RaceId = team.RaceId;
+            PlayerList = playerReadModels;
+        }
+    }
+
+    public class TeamOverviewHto
+    {
+        public string TeamTeamName { get; }
+        public Guid TeamTeamId { get; }
+        public string TeamTrainerName { get; }
+        public string RaceId { get; }
+        public bool TeamIsFinished { get; }
+
+        public TeamOverviewHto(TeamReadModel team)
+        {
+            TeamTeamName = team.TeamName;
+            TeamTeamId = team.TeamId;
+            TeamTrainerName = team.TrainerName;
+            RaceId = team.RaceId;
+            TeamIsFinished = team.IsFinished;
         }
     }
 }
