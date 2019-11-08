3fa95e166f96a62d99822bc9b5a8234f07f23039
Simon Heiss <simonheiss87@gmail.com>
2019-11-08 09:08:04 +0100

    removed comments

--- ServiceConfig/ServiceConfig.cs
+++ ServiceConfig/ServiceConfig.cs
@@ -23,15 +23,6 @@ namespace ServiceConfig
         public static Uri SeasonReadHost { get; } = new Uri("https://seasons-readhost.herokuapp.com/");
         public static Uri TeamReadHost { get; } = new Uri("https://teams-readhost.herokuapp.com/");
 
-
-//        public static ServiceBaseAddressCollection ServiceAdresses => new ServiceBaseAddressCollection
-//        {
-//            new Uri("http://localhost:5001"),
-//            new Uri("http://localhost:5002"),
-//            new Uri("http://localhost:5003"),
-//            new Uri("http://localhost:5004"),
-//            new Uri("http://localhost:5007"),
-//        };
 //        public static Uri MatchHost { get; } = new Uri("http://localhost:5003");
 //        public static Uri TeamHost { get; } = new Uri("http://localhost:5001");
 //        public static Uri PlayerHost { get; }  = new Uri("http://localhost:5002");
