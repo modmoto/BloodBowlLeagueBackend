f785745b79b4ca5ef37abf0cfa938c703ba1ea26
Simon Heiss <simonheiss87@gmail.com>
2019-11-12 00:08:41 +0100

    removed player stat field

--- ReadHosts/Teams.ReadHost/Players/PlayerReadModel.cs
+++ ReadHosts/Teams.ReadHost/Players/PlayerReadModel.cs
@@ -16,7 +16,6 @@ namespace Teams.ReadHost.Players
         IHandle<PlayerMadeTouchdown>,
         IHandle<PlayerWasNominatedMostValuablePlayer>
     {
-        private PlayerStats PlayerStats;
         public Guid PlayerId { get; private set; }
         public Guid TeamId { get; private set; }
         public string PlayerTypeId { get; private set; }
@@ -39,7 +38,6 @@ namespace Teams.ReadHost.Players
             TeamId = domainEvent.TeamId;
             PlayerTypeId = domainEvent.PlayerTypeId;
             PlayerConfig = domainEvent.PlayerConfig;
-            PlayerStats = domainEvent.PlayerConfig.PlayerStats;
             var startingSkills = Skills.ToList();
             startingSkills.AddRange(domainEvent.PlayerConfig.StartingSkills.Select(s => s.SkillId));
             Skills = startingSkills;
