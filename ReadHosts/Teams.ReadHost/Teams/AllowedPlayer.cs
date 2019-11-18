24cdeff311b93be1720d97394d87482f35efead4
Simon Heiss <simonheiss87@gmail.com>
2019-11-18 18:22:52 +0100

    allowerd player as readmodel

--- ReadHosts/Teams.ReadHost/Teams/AllowedPlayer.cs
+++ ReadHosts/Teams.ReadHost/Teams/AllowedPlayer.cs
@@ -1,16 +1,23 @@
-﻿namespace Teams.ReadHost.Teams
+﻿using System.Collections.Generic;
+using Teams.ReadHost.Players;
+using Teams.ReadHost.Races;
+
+namespace Teams.ReadHost.Teams
 {
     public class AllowedPlayer
     {
-        public AllowedPlayer(string playerTypeId, int maximumPlayers, GoldCoins cost)
-        {
-            PlayerTypeId = playerTypeId;
-            MaximumPlayers = maximumPlayers;
-            Cost = cost;
-        }
+        public string PlayerTypeId { get; set; }
+        public int MaximumPlayers { get; set; }
+        public GoldCoins Cost { get; set; }
+        public PlayerStats PlayerStats { get; set; }
+        public IEnumerable<Skill> StartingSkills { get; set; }
+        public IEnumerable<SkillType> SkillsOnDefault { get; set; }
+        public IEnumerable<SkillType> SkillsOnDouble { get; set; }
+    }
 
-        public string PlayerTypeId{ get; set; }
-        public int MaximumPlayers{ get; set; }
-        public GoldCoins Cost{ get; set; }
+    public class Skill
+    {
+        public string SkillId { get; set; }
+        public SkillType SkillType { get; set; }
     }
 }
