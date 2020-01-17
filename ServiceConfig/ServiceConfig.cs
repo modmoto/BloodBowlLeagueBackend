8e13074fd6c4a7752f7ca29f0e40f283e55a4396
Simon Heiss <simonheiss87@gmail.com>
2020-01-17 22:29:31 +0100

    removed sonar warnings

--- ServiceConfig/ServiceConfig.cs
+++ ServiceConfig/ServiceConfig.cs
@@ -3,16 +3,13 @@ using Microwave;
 
 namespace ServiceConfig
 {
-    public class ServiceConfiguration
+    public static class ServiceConfiguration
     {
         private static string TeamHostPort = ":6001";
         private static string PlayerHostPort = ":6002";
         private static string MatchHostPort = ":6003";
         private static string SeasonHostPort = ":6004";
         private static string RaceHostPort = ":6005";
-
-        // private static string SeasonReadHostPort = ":5001";
-        // private static string TeamReadHostPort = ":5000";
 
         public static ServiceBaseAddressCollection ServiceAdressesFrom(string baseAdress)
         {
