6f2e358bb17872398d18df9fb601afe094ceb3aa
Simon Heiss <simonheiss87@gmail.com>
2020-01-17 21:57:01 +0100

    new ports

--- ServiceConfig/ServiceConfig.cs
+++ ServiceConfig/ServiceConfig.cs
@@ -5,13 +5,13 @@ namespace ServiceConfig
 {
     public class ServiceConfiguration
     {
-        private static string MatchHostPort = ":5003";
-        private static string TeamHostPort = ":5001";
-        private static string PlayerHostPort = ":5002";
-        private static string SeasonHostPort = ":5004";
-        private static string RaceHostPort = ":5007";
+        private static string TeamHostPort = ":6001";
+        private static string PlayerHostPort = ":6002";
+        private static string MatchHostPort = ":6003";
+        private static string SeasonHostPort = ":6004";
+        private static string RaceHostPort = ":6005";
 
-        // private static string SeasonReadHostPort = ":5006";
+        // private static string SeasonReadHostPort = ":5001";
         // private static string TeamReadHostPort = ":5000";
 
         public static ServiceBaseAddressCollection ServiceAdressesFrom(string baseAdress)
