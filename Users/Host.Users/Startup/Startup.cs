e3705f6ea7f649d181c5f1fbba482c142d53f2ad
Simon Heiss <simonheiss87@gmail.com>
2020-03-02 18:45:27 +0100

    added host identity server

---
+++ Users/Host.Users/Startup/Startup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Host.Users.Startup
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(IdentityConfig.Apis)
+                .AddInMemoryClients(IdentityConfig.Clients);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseIdentityServer();
+            app.UseRouting();
+        }
+    }
+}
